507f384ef8d64c4cea954fd74a8ba0c5d7b5e5c3
danbuis <dan.buis88@gmail.com>
2020-12-08 08:43:46 -0500

    Add some constants for measurements
    This is copied over as a base from a different project.

--- src/GameConstants.java
+++ src/GameConstants.java
@@ -4,4 +4,32 @@ public class GameConstants {
      * different types of objects.
      */
 
+    // Dimensions in millimeters
+    /*
+     * Ranges and distances were sourced from FFG user leerat at
+     * https://community.fantasyflightgames.com/topic/173990-exact-measurements-on-range-ruler/
+     * 13/2/2017
+     */
+    public static final float distance1 = 71.5f;
+    public static final float distance2 = 125;
+    public static final float distance3 = 185;
+    public static final float distance4 = 245;
+    public static final float distance5 = 305;
+
+    public static final float rangeClose = 123;
+    public static final float rangeMedium = 187;
+    public static final float rangeLong = 305;
+    /*
+     * Ship sizes were sourced from
+     * http://starwars-armada.wikia.com/wiki/Size_Class
+     * 13/2/2017
+     */
+    public static final float shipSmallWidth = 41;
+    public static final float shipSmallLength =71;
+    public static final float shipMediumWidth = 61;
+    public static final float shipMediumLength = 102;
+    public static final float shipLargeWidth = 76;
+    public static final float shipLargeLength = 129;
+
+    public static final float shipBaseRails = 2;
 }
