ee923ac3de483f0d763fd1f0d653864a75174737
danbuis <dan.buis88@gmail.com>
2020-12-08 08:36:42 -0500

    Create GameConstants.java
    Create stub file with some documentation comments

---
+++ src/GameConstants.java
@@ -0,0 +1,7 @@
+public class GameConstants {
+    /**
+     * This is going to be a static class object to contain common values that need to be accessible across many
+     * different types of objects.
+     */
+
+}
