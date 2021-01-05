174c9f81ebfc1754bb92c5170f206ff4baec73ac
danbuis <dan.buis88@gmail.com>
2021-01-04 19:52:46 -0500

    Add squadron constructor
    A squadron parser will read the file and buile a squadron object
    
    Added final keyword to the fields that won't ever need to be modified.

--- src/gameComponents/Squadron.java
+++ src/gameComponents/Squadron.java
@@ -1,18 +1,35 @@
 package gameComponents;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class Squadron {
 
-    private String type;
-    private String name;
-    private boolean unique;
+    private final String type;
+    private final String name;
+    private final boolean unique;
     private int currentHealth;
-    private int fullHealth;
-    private int maxSpeed;
-    private String antiShipDice;
-    private String antiSquadronDice;
-    private ArrayList<String> keywords;
-    private int pointsValue;
+    private final int fullHealth;
+    private final int maxSpeed;
+    private final String antiShipDice;
+    private final String antiSquadronDice;
+    private final ArrayList<String> keywords;
+    private final int pointsValue;
     private ArrayList<String> defenseTokens;
+
+    public Squadron(String type, String name, boolean unique, int fullHealth,
+                    int maxSpeed, String antiShipDice, String antiSquadronDice,
+                    ArrayList<String> keywords, int pointsValue, ArrayList<String> defenseTokens){
+        this.type = type;
+        this.name = name;
+        this.unique = unique;
+        this.fullHealth = fullHealth;
+        this.currentHealth = fullHealth;
+        this.maxSpeed = maxSpeed;
+        this.antiShipDice = antiShipDice;
+        this.antiSquadronDice = antiSquadronDice;
+        this.keywords = keywords;
+        this.pointsValue = pointsValue;
+        this.defenseTokens = defenseTokens;
+    }
 }
