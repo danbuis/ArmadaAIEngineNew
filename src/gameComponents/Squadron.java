ee98de6066cf2fd88656ae5aac1014cdcf1545f5
danbuis <dan.buis88@gmail.com>
2021-01-04 19:45:52 -0500

    Create Squadron.java
    Add fields corresponding to the lines in the text file.
    
    These are all private because the squadron shouldn't expose its private data to other game Components.  The other items will query it for its data.  This is to make it harder for outside objects to modify the state of this object.

---
+++ src/gameComponents/Squadron.java
@@ -0,0 +1,18 @@
+package gameComponents;
+
+import java.util.ArrayList;
+
+public class Squadron {
+
+    private String type;
+    private String name;
+    private boolean unique;
+    private int currentHealth;
+    private int fullHealth;
+    private int maxSpeed;
+    private String antiShipDice;
+    private String antiSquadronDice;
+    private ArrayList<String> keywords;
+    private int pointsValue;
+    private ArrayList<String> defenseTokens;
+}
