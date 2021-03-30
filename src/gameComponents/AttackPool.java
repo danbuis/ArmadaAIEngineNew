7ec7dc59ace98df2174865d8912a71afbb83651c
danbuis <dan.buis88@gmail.com>
2021-03-30 13:18:34 -0400

    Stub out some fields and public functions
    This should be a decent summary of the stuff that happens in dice modifications for now.

--- src/gameComponents/AttackPool.java
+++ src/gameComponents/AttackPool.java
@@ -1,4 +1,32 @@
 package gameComponents;
 
 public class AttackPool {
+    private boolean spentBrace = false;
+    private boolean spentEvade = false;
+    private boolean spentContain = false;
+    private boolean spentScatter = false;
+    private boolean spentRedirect = false;
+    private int currentRolledDamage;
+    private String currentPool;
+
+
+    public AttackPool(){
+
+    }
+
+    public void rollPool(){
+
+    }
+
+    public void rerollDie(){
+
+    }
+
+    public void addDie(){
+
+    }
+
+    public void cancelDie(){
+
+    }
 }
