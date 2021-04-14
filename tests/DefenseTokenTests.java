7188177e91b1ab8818a74af5af5015961aaf7cae
danbuis <dan.buis88@gmail.com>
2021-04-14 19:51:56 -0400

    Stub out test file

---
+++ tests/DefenseTokenTests.java
@@ -0,0 +1,36 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class DefenseTokenTests {
+
+    @Test
+    public void testConstructor(){
+
+    }
+
+    @Test
+    public void testExhaust(){
+
+    }
+
+    @Test
+    public void testDiscard(){
+
+    }
+
+    @Test
+    public void testSpend(){
+
+    }
+
+    @Test
+    public void ready(){
+
+    }
+
+    @Test
+    public void recover(){
+        
+    }
+
+}
