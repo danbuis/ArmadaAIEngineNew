1f9402ed223d3511d23a1975b105379326b91cb9
danbuis <dan.buis88@gmail.com>
2021-04-15 16:32:30 -0400

    Revert "Revert "Squadron construction expects DefenseToken objects""
    This reverts commit 70c05fd1bc8fd509e0de9663a65a2c2399e9db65.

--- src/engine/parsers/SquadronFactory.java
+++ src/engine/parsers/SquadronFactory.java
@@ -1,5 +1,6 @@
 package engine.parsers;
 
+import gameComponents.DefenseTokens.DefenseToken;
 import gameComponents.Squadrons.Squadron;
 
 import java.io.File;
@@ -23,7 +24,7 @@ public class SquadronFactory {
     private String antiSquadronDice;
     private ArrayList<String> keywords;
     private int points;
-    private ArrayList<String> defenseTokens = null;
+    private ArrayList<DefenseToken> defenseTokens = null;
 
     private final int NUMBER_OF_FIELDS = 11;
 
@@ -96,7 +97,13 @@ public class SquadronFactory {
                             this.defenseTokens = new ArrayList<>();
                         }else{
                             String[] defenseTokenArray = value.split(" ");
-                            this.defenseTokens = new ArrayList<>(Arrays.asList(defenseTokenArray));
+                            ArrayList<DefenseToken> tokens = new ArrayList<>();
+
+                            for(String token: defenseTokenArray){
+                                ParsingUtils.checkValidDefenseToken(token);
+                                tokens.add(new DefenseToken(token));
+                            }
+                            this.defenseTokens = tokens;
                         }
                         break;
                 } // end switch block
