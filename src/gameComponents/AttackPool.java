3c4560c17eed4df85aa7cd72a8f6629eaf7b7ebe
danbuis <dan.buis88@gmail.com>
2021-04-18 21:03:10 -0400

    Remove defense token bools
    AttackPool shouldn't really keep track of this anyway

--- src/gameComponents/AttackPool.java
+++ src/gameComponents/AttackPool.java
@@ -10,11 +10,6 @@ import java.util.ArrayList;
  */
 
 public class AttackPool {
-    private boolean spentBrace = false;
-    private boolean spentEvade = false;
-    private boolean spentContain = false;
-    private boolean spentScatter = false;
-    private boolean spentRedirect = false;
     private int currentRolledDamage;
     private ArrayList<Die> currentPool;
     private boolean critEffect;
