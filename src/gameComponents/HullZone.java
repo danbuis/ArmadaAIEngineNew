7b3cb51613f327e3fcc166dd9ff552ab3e22903e
boredompwndu <thombuis@gmail.com>
2021-04-29 20:11:23 -0400

    hull zones can gain/lose shields, such as from card effects

--- src/gameComponents/HullZone.java
+++ src/gameComponents/HullZone.java
@@ -20,8 +20,9 @@ public class HullZone {
     private int blueBattery;
     private int blackBattery;
 
-    //The shield value of the hull zone
+    //The current and maximum shield value of the hull zone. In most cases these start identical
     private int shields;
+    private int shieldsMaximum;
 
     //The Line of sight offset relative to ship center for a HullZone
     private Vector2f LoSOffset;
@@ -58,6 +59,7 @@ public class HullZone {
         this.blueBattery = blueBattery;
         this.blackBattery = blackBattery;
         this.shields = shields;
+        this.shieldsMaximum = shields;
         this.LoSOffset = LoSOffset;
         this.leftArcOffset = leftArcOffset;
         this.leftArcAngle = leftArcAngle;
@@ -65,6 +67,11 @@ public class HullZone {
         this.rightArcOffset = rightArcOffset;
     }
 
+    /**
+     * Handles taking shield damage from an attack
+     * @param toTake the damage the hull zone is to take
+     * @return damage suffered in excess of shields. this should go to hull damage
+     */
     public int sufferDamage(int toTake) {
         if (toTake > shields) { //If we need to take more damage than shields remaining
             shields = 0;
@@ -73,6 +80,30 @@ public class HullZone {
             shields -= toTake;
         } else shields = 0; //if we reach this point we are taking exactly as much damage as shields remaining
         return 0; //in cases 2 and 3, all damage has been mitigated and nothing needs ot be suffered on hull, so this lives outside the if/else blocks
+    }
+
+    /**
+     * How a ship gains shields, such as from card effects or spending engineering points
+     * @param toGain the number of shields a hull zone will attempt to gain
+     * @return the number of shields actually gained
+     */
+    public int gainShield(int toGain){
+        int shieldsRemaining = Math.min(shields+toGain,shieldsMaximum);
+        int shieldsToGain  =  shieldsRemaining-shields;
+        shields = shieldsRemaining;
+        return shieldsToGain;
+    }
+
+    /**
+     * How a ship loses shields, such as from card effects or spending engineering points
+     * @param toRemove the number of shields a hull zone will attempt to remove
+     * @return the number of shields actually removed
+     */
+    public int removeShield(int toRemove){
+        int shieldsRemaining = Math.max(shields-toRemove,0);
+        int shieldsToRemove = shields-shieldsRemaining;
+        shields = shieldsRemaining;
+        return shieldsToRemove;
     }
 
     public Facing getFacing() {
