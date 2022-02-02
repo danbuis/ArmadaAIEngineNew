bdb4f80c7ca5d6c8c982b2ddb9ea45fe15a00f37
danbuis <dan.buis88@gmail.com>
2022-02-02 11:54:54 -0500

    Add home screen functionality

--- src/GUI/screens/HomeScreen.java
+++ src/GUI/screens/HomeScreen.java
@@ -2,18 +2,22 @@ package GUI.screens;
 
 import BBDGameLibrary.GUI.BBDFont;
 import BBDGameLibrary.GUI.BBDTextLine;
+import BBDGameLibrary.GameEngine.GameItem;
+import BBDGameLibrary.GameEngine.MouseInput;
 import BBDGameLibrary.OpenGL.Window;
+import engine.ArmadaGame;
+import org.joml.Vector2d;
 
 import java.io.FileNotFoundException;
 
-public class HomeScreen extends Screen {
+public class HomeScreen extends Screen implements ScreenWidget{
     private BBDFont font;
     private BBDTextLine textTestGame;
     private BBDTextLine textCivilWarDemo;
     private BBDTextLine textCloneWarDemo;
 
-    public HomeScreen(Window window){
-        super(window);
+    public HomeScreen(Window window, ArmadaGame parent){
+        super(window, parent);
         try {
             font = new BBDFont("assets/text/Arial_Bold_White.bmp", "assets/text/Arial_Bold_White.csv");
         } catch(FileNotFoundException e){
@@ -24,12 +28,39 @@ public class HomeScreen extends Screen {
         textCloneWarDemo = new BBDTextLine(font, 100, "Clone War Era Demo", 10);
 
         textTestGame.setPosition(-textTestGame.getTotalWidth()/2, (float) (windowHeight * 0.25));
-        textCivilWarDemo.setPosition(-textCivilWarDemo.getTotalWidth()/2, 0);
-        textCloneWarDemo.setPosition(-textCloneWarDemo.getTotalWidth()/2, (windowHeight * -0.25f));
+        textCloneWarDemo.setPosition(-textCloneWarDemo.getTotalWidth()/2, 0);
+        textCivilWarDemo.setPosition(-textCivilWarDemo.getTotalWidth()/2, (windowHeight * -0.25f));
 
         this.addText(textTestGame);
         this.addText(textCivilWarDemo);
         this.addText(textCloneWarDemo);
+
+        this.addClickableText(textCloneWarDemo);
+        this.addClickableText(textTestGame);
+        this.addClickableText(textCivilWarDemo);
+
     }
-    
+
+    @Override
+    public void handleClick(Vector2d mousePos, Window window) {
+        System.out.println(mousePos);
+        GameItem clickedItem = selector.selectItemByMouse(clickables, window, mousePos, parent.getCamera(), 0.001f);
+        if (textTestGame.getTextItemList().contains(clickedItem)){
+            System.out.println("Clicked a test char");
+            parent.changeScreens(ScreenState.TEST);
+        }else if (textCivilWarDemo.getTextItemList().contains(clickedItem)){
+            System.out.println("Clicked a civil war char");
+            parent.changeScreens(ScreenState.GAME_SMALL);
+        }else if (textCloneWarDemo.getTextItemList().contains(clickedItem)){
+            System.out.println("Clicked a clone char");
+            parent.changeScreens(ScreenState.GAME_SMALL);
+        }else{
+            System.out.println("Clicked a NONE char");
+        }
+    }
+
+    @Override
+    public void update(float v, MouseInput mouseInput) {
+
+    }
 }
