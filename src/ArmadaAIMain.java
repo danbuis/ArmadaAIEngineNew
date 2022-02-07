36fc5d3aeb6b696f3126fd353e16e8bf99f21658
danbuis <dan.buis88@gmail.com>
2022-02-07 12:29:58 -0500

    Initialize the static solid colors object
    Do this for a single central place that solid color data is stored

--- src/ArmadaAIMain.java
+++ src/ArmadaAIMain.java
@@ -2,13 +2,14 @@ import BBDGameLibrary.GameEngine.Engine;
 import BBDGameLibrary.GameEngine.GameComponent;
 import BBDGameLibrary.OpenGL.Window;
 import engine.ArmadaGame;
+import resources.SolidColorShaders;
 
 public class ArmadaAIMain {
 
 	public static void main(String[] args) {
 		try {
 			GameComponent gameLogic = new ArmadaGame();
-
+			SolidColorShaders.initialize();
 			Engine gameEng = new Engine("Star Wars Armada AI",
 					1800, 980, true, new Window.WindowOptions(), gameLogic);
 			gameEng.run();
--- src/resources/SolidColorShaders.java
+++ src/resources/SolidColorShaders.java
@@ -5,11 +5,16 @@ import java.util.HashMap;
 public class SolidColorShaders {
     public static HashMap<String, float[]> solidShadervalues = new HashMap<String, float[]>();
 
-    public void createSolidColor(String key, float[] input){
+    public static void initialize(){
+        createSolidColor("black", new float[]{0f, 0f, 0f, 1f});
+        createSolidColor("white", new float[]{1f, 1f, 1f, 1f});
+    }
+
+    public static void createSolidColor(String key, float[] input){
         solidShadervalues.put(key, input);
     }
 
-    public float[] getSolidColor(String key){
+    public static float[] getSolidColor(String key){
         return solidShadervalues.get(key);
     }
 }
