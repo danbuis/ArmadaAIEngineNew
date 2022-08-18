5adeca65974862ba3c337868071100b5a211fc43
danbuis <dan.buis88@gmail.com>
2022-08-18 12:18:51 -0400

    Tweak creation of template
    Do this to put data in the right place

--- src/components/ship/ManeuverTemplate.java
+++ src/components/ship/ManeuverTemplate.java
@@ -6,21 +6,18 @@ public class ManeuverTemplate {
     public ManeuverTemplate(String maneuverChart) {
         String[] speedStrings = maneuverChart.split(" ");
         int[][] navChartArray = new int[5][0]; // [speed][joint clicks]
-        for (int i = 1; i <= speedStrings.length - 1; i++) {
+        for (int i = 0; i <= speedStrings.length - 1; i++) {
             if (!speedStrings[i].equals("-")) {
                 // split the column into single digits
                 String[] navColumn = speedStrings[i].split("");
 
                 // make column the appropriate length
-                navChartArray[i] = new int[navColumn.length];
+                navChartArray[i+1] = new int[navColumn.length]; //+1 to make sure that we can call with the actual speed and avoid 0-indexed nonsense
 
                 // fill in specific numbers
-                for (int j = navColumn.length - 1; j >= 0; j--) {
+                for (int j = 0; j <= navColumn.length - 1; j++) {
                     String digitToAdd = navColumn[j];
-                    if (digitToAdd.equals("-")) {
-                        digitToAdd = "0";
-                    }
-                    navChartArray[i][navColumn.length - 1 - j] = Integer.parseInt(digitToAdd);
+                    navChartArray[i+1][j] = Integer.parseInt(digitToAdd);
                 }
             }
         }
@@ -30,8 +27,14 @@ public class ManeuverTemplate {
     public boolean validateManeuver(int[] clicks, int extraClicks){
         int extrasUsed = 0;
         int speed = clicks.length;
+        if(!validSpeed(speed)){
+            return false;
+        }
         int[] availableClicks = navChart[speed];
         for (int i = 0; i < speed ; i++) {
+            if(Math.abs(clicks[i]) > 2){
+                return false;
+            }
             if(Math.abs(clicks[i]) > availableClicks[i]) {
                 extrasUsed++;
             }
@@ -54,4 +57,6 @@ public class ManeuverTemplate {
         }
         return base;
     }
+
+    public String getOriginalData() { return this.originalData;}
 }
